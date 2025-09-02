13704026f0449b711d6d39eb091e648eddebe1e8
Uthra <uthra2407@gmail.com>
2025-09-02 20:32:17 +0530

    Added functionality for delete, edit , undo

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    #Add Task
     path('addTask/', views.addTask, name='addTask'),
+    #Mark as Completed
     path('completed/<int:pk>/', views.completed, name='completed'),
+    #Mark as not Completed
+    path('undone/<int:pk>/', views.undone, name='undone'),
+    #Delete 
+    path('delete/<int:pk>/',views.delete, name='delete'),
+    #Edit
+    path('edit/<int:pk>/',views.edit, name='edit'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -25,3 +25,28 @@ def completed(request, pk):
     t.save()
     return redirect('/')
     # return HttpResponse(pk)
+    
+def undone(request,pk):
+    t=get_object_or_404(Task,pk=pk)
+    t.is_completed =False
+    t.save()
+    return redirect('/')
+
+def delete(request,pk):
+    d_t=get_object_or_404(Task,pk=pk)
+    d_t.delete()
+    return redirect('/')
+
+def edit(request,pk):
+    get_task = get_object_or_404(Task,pk=pk)
+    if request.method == 'POST':
+        new_task= request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('/')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+    return render(request, 'edit.html', context)
+    
